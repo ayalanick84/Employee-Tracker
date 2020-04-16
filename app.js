f130ce980b9003a891e443eb0d2fa4e1dc061442
Nicolas Ayala <ayalanick84@gmail.com>
2020-04-15 22:28:19 -0700

    Got add role and almost add employee

--- app.js
+++ app.js
@@ -77,6 +77,9 @@ const nextAction = () => {
             process.exit(1);
         });
 };
+// const deps = [];
+// const roles = [];
+// const employees = [];
 function addDep() {
     return inquirer
         .prompt([
@@ -89,19 +92,129 @@ function addDep() {
         .then((answer) => {
             // when finished prompting, insert a new item into the db with that info
             return connection.query(
-              "INSERT INTO department SET ?",
-              {
-                name: answer.addDep
-              },
-              (err) => {
-                if (err) {
-                  throw err;
-                }
-                console.log("Your department was created successfully!");
-                // re-prompt the user for if they want to bid or post
-                return nextAction();
-              })})
-      
-
-
+                "INSERT INTO department SET ?",
+                {
+                    dep_name: answer.addDep
+                },
+                function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log("Success");
+                    // re-prompt the user for if they want to bid or post
+                    return nextAction();
+                })
+        })
 }
+function addRole() {
+    // query the database for all items being auctioned
+    return connection.query("SELECT * FROM department", (err, results) => {
+        if (err) {
+            throw err;
+        }
+        const depNames = results.map((row) => {
+            return {
+                name: row.dep_name,
+                value: row.position,
+            }
+        });
+        // once you have the items, prompt the user for which they'd like to bid on
+        return inquirer
+            .prompt([
+                {
+                    name: "dep",
+                    type: "list",
+                    choices: depNames,
+                    message: "What department is it in?",
+                },
+                {
+                    name: "title",
+                    type: "input",
+                    message: "What is the name of the role?",
+                }, {
+                    name: "salary",
+                    type: "input",
+                    message: "What is the salary"
+                }
+            ])
+            .then((answers) => {
+                return connection.query(
+                    "INSERT INTO role (dep_id,title, salary) VALUES ( ?, ?, ?)",
+                    [answers.dep, answers.title, answers.salary],
+                    function (err, res) {
+                        if (err) {
+                            throw err;
+                        }
+                        console.log("Success");
+                        // re-prompt the user for if they want to bid or post
+                        return nextAction();
+                    })
+            })
+    })
+}
+function addEmp() {
+    // query the database for all items being auctioned
+    return connection.query("SELECT * FROM role", (err, results) => {
+        if (err) {
+            throw err;
+        }
+        const roleNames = results.map((row) => {
+            return {
+                name: row.title,
+                value: row.position,
+            }
+        });
+        connection.query(
+            "SELECT CONCAT(employee.first_name, ' ', employee.last_name) AS manager, employee.position FROM employee",
+            (err, results) => {
+                if (err) {
+                    throw err;
+                }
+                const managers = results.map((row) => {
+                    return {
+                        name: row.manager,
+                        value: row.id
+                    };
+                });
+                managers.push("They have no manager")
+                // once you have the items, prompt the user for which they'd like to bid on
+                return inquirer
+                    .prompt([
+                        {
+                            name: "role",
+                            type: "list",
+                            choices: roleNames,
+                            message: "What is their role?",
+                        },
+                        {
+                            name: "manager",
+                            type: "list",
+                            message: "Who is their manager?",
+                            choices: managers
+                        }, {
+                            name: "firstName",
+                            type: "input",
+                            message: "What is their first name?"
+                        }, {
+                            name: "lastName",
+                            type: "input",
+                            message: "What is their last name"
+                        }
+                    ])
+                    .then((answers) => {
+                        return connection.query(
+                            "INSERT INTO employee (role_id,manager_id,first_name,last_name) VALUES ( ?,?, ?, ?)",
+                            [answers.role, , answers.manager, answers.firstName, answers.lastName],
+                            function (err, res) {
+                                if (err) {
+                                    throw err;
+                                }
+                                console.log("Success");
+                                // re-prompt the user for if they want to bid or post
+                                return nextAction();
+                            })
+                    })
+            })
+    })
+}
+
